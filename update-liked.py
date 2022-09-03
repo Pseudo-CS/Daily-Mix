3358dc03f5b062381759ccecd78cc1f811b55bae
Pseudo-CS <saandeepsijo@gmail.com>
2022-09-03 21:06:56 +0530

    final

--- update-liked.py
+++ update-liked.py
@@ -1,19 +1,21 @@
 import requests
 
-token = 'BQCuIF4u-B2khNnAEqsf4zRqdITZ0zCblzDBNpgESr8kK-wUG6WdBkbmUsdJ0vZzvxoBZI_4NxnoEp3ychVFgMMqyMtc4l7_KbHr4xJyYshBczxx2ynzIr3FyxZywKc6-8Ij5dKoaPnGDrVcKBRTasivlg2maAhSwcM4uvh6Ox3T7yw8xqXofl35a3CyoYaH-gDQpJSArLoR'
+token = 'BQAj_LSRq9p5tGZudJa-cPm2Dgh_q8UqaJLJ-ECvtDS8R-dXa87GGU_knJVgzvLUE1VrN90-2OW1LXxji79rKG8XqljTVdLoANjrk0ZOqg15T53O1nwtSW8TSq0cV-h79zLFOyRGgZsPtNa_uUHXdFxW39ROosdJwDGz-3-wSycvvHcR6R5RPHQisTI33wx9osaqDiJive3s'
 headin = {'Authorization': 'Bearer '+token}
 
-# liked_songs = open('Daily-Mix/liked-songs', 'a')
-liked_songs = open('Daily-Mix/liked-songs', 'a+')
+liked_songs = open('Daily-Mix/liked-songs', 'r+')
 
 d = requests.get('https://api.spotify.com/v1/me/tracks?limit=5&offset=0', headers=headin)
 print(d.status_code, d.reason)
     
+myfile = liked_songs.read()
 
 for i in d.json()['items']:
     entry = i['track']['name'] + ' - ' + i['track']['artists'][0]['name']
-    if(entry in liked_songs.read()):
+
+    if(entry in myfile):
         break
-    liked_songs.write('\n' + entry)
+    else:
+        liked_songs.write('\n' + entry)
 
 liked_songs.close()
