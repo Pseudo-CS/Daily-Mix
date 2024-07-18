02b5c07d50941602f2354bf26766b0196082c6d1
Pseudo <73516975+Pseudo-CS@users.noreply.github.com>
2024-07-18 22:46:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-#https://accounts.spotify.com/authorize?client_id=eda12a570bbc45acb1838434b482f684&response_type=code&redirect_uri=http://localhost:8000&scope=playlist-modify-public+playlist-modify-private 
-
 import base64
 import requests
 
-client_id = 'eda12a570bbc45acb1838434b482f684'
-client_secret = '0cb0b76eb9594ebcac2f507f01b139d9'
+client_id = ''
+client_secret = ''
 final = client_id + ':' + client_secret
 final = str(base64.b64encode(bytes(final, 'utf-8'))).strip('b\'').strip('\'')
 
@@ -12,7 +10,7 @@ final = str(base64.b64encode(bytes(final, 'utf-8'))).strip('b\'').strip('\'')
 headers = {'Authorization': 'Basic ' + final, 
             'Content-Type': 'application/x-www-form-urlencoded'}
 data = {'grant_type': 'authorization_code',
-        'code': 'AQC3aBuMIyMk9_LPxTf5EWLS-3N5NTwg3vk5sFnk_V9x3dP7T3shoBGL1i00jkLOLz2K0YwRy_YozqlK5-FHCVu0XvV3kirB19VKxnhtiP1XtyoSZxoTBWj0f6UG20EZC1TPS4kZRgyr4bKwhuNZ3WeT1ZhCp-V9vn2nbsZw6XGDhJb_YkpunO2D-7Ave9bkDdCnLrFIqN0rHA1CJzpZagKPbt8ZRGNAqng',
+        'code': '',
         'redirect_uri': 'http://localhost:8000'}
 
 
@@ -33,8 +31,6 @@ def access_token():
 acc = access_token().json().get('access_token')
 headin = {'Authorization': 'Bearer ' + acc}
 
-#BQAUx-db-wYLr-HQlJPN0yxtXElo6v8VVp8UAyRmRRjJC4isL3UxUDHTGEgIG9eKs3KT4h08vLj7w9V8xifxZYnh2CgWexZycJ2kxtSJJiitQdx21VgileK7gLkojdwjyCsiPnFnviyn_C0js6MOmU7LwZW6B4DNujUqZRLE0-JmYndIprOahDBoDAyh6xh_wCIkzH-McCgtH--gscarfizmxR4T5rhKvdWZpLrgnQeV6OyFzRmTK48DyLQcPzc
-#AQBmH68mYqJdsuAleUpn93FeyqH6nKcdgJpmVzwtnLH7SnfKq0_WIFoDafvZVVfM-mpuXTjTkjkT8QDsRA9PHXSYWYHyxaCqs2jxPoR5gIsWq6b4cVAUVHGk7erQ0kD1-lQ
 
 
 def pull_mix(mix_link):
